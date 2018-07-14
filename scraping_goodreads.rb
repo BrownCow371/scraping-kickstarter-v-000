bc462b0484e2680600db6b242fcca9316bb30caf
Jamie Brown <jmbrowncow@gmail.com>
2018-07-14 01:25:39 +0000

    automatically backed up by learn

--- scraping_goodreads.rb
+++ scraping_goodreads.rb
@@ -10,14 +10,14 @@ binding.pry
 
 
   #title =
-  goodreads_fiction.css(".bigBoxBody.bookBox script").each do |book|
-    title = book.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    }
-   end
+  # goodreads_fiction.css(".bigBoxBody.bookBox script").each do |book|
+  #   title = book.css("h2.bbcard_name strong a").text
+  #   projects[title.to_sym] = {
+  #     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+  #     :description => project.css("p.bbcard_blurb").text,
+  #     :location => project.css("ul.project-meta span.location-name").text,
+  #     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  #   }
+  #  end
    books
 end
