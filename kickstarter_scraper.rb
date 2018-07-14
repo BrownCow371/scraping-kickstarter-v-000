e1aeafee391c7627ef5b4e8ffd0a835b3daf9fac
Jamie Brown <jmbrowncow@gmail.com>
2018-07-14 01:32:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,25 +1,39 @@
+
+# require libraries/modules here
 require 'nokogiri'
-require 'open-uri'
 require 'pry'
 
-def create_book_hash
-  books = {}
-  books
-  binding.pry
+def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
 
-  # goodreads_fiction = Nokogiri::HTML(open("https://www.goodreads.com/genres/fiction/"))
+  kickstarter = Nokogiri::HTML(html)
+  #binding.pry
+  projects = {}
 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+   end
+   projects
 
-
-  #title =
-  # goodreads_fiction.css(".bigBoxBody.bookBox script").each do |book|
-  #   title = book.css("h2.bbcard_name strong a").text
-  #   projects[title.to_sym] = {
-  #     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-  #     :description => project.css("p.bbcard_blurb").text,
-  #     :location => project.css("ul.project-meta span.location-name").text,
-  #     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  #   }
-  #  end
-
+  # projects = kickstarter.css(".project-card")
+  # big_project_hash={}
+  # projects.each do |project|
+  #   project_hash = {}
+  #   title = project.css("a").text
+  #   big_project_hash[title] = project_hash
+  #   big_project_hash[title][:location] = project.css(".location-name").text
+  #   big_project_hash[title][:description] = project.css("p").text
+  #   big_project_hash[title][:image_link] = project.css("div.project-thumbnail a img").attribute("src").value
+  #   big_project_hash[title][:percent_funded] = project.css(".first-funded").text.gsub("%", "").to_i
+  #
+  # end
+  # big_project_hash
 end
+
+create_project_hash
